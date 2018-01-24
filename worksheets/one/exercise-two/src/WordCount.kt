b27c06b90ae82a19409af64b68db1120f5da4e4f
vdavios <vas.davios@gmail.com>
2018-01-24 13:30:54 +0000

    Ex02

--- worksheets/one/exercise-two/src/WordCount.kt
+++ worksheets/one/exercise-two/src/WordCount.kt
@@ -1,7 +1,16 @@
 object WordCount {
 
     fun phrase(phrase: String): Map<String, Int> {
-        return HashMap<String, Int>()
+
+        val myListOfStrings = phrase.toLowerCase()
+                .replace(Regex("[^\\w']"), " ")
+                .trim().split(Regex("\\s+"))
+        return removeQuotation(myListOfStrings).groupBy { it }.mapValues { it.value.size }
+
+    }
+
+    private fun removeQuotation(list: List<String>): List<String> {
+        return list.map{it.removePrefix("'").removeSuffix("'")}
     }
 
 }
