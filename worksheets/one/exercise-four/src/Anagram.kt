b55437fa2962ff816e87fabdb3269331fe6a5446
vdavios <vas.davios@gmail.com>
2018-01-24 14:26:19 +0000

    Completed Ex04

--- worksheets/one/exercise-four/src/Anagram.kt
+++ worksheets/one/exercise-four/src/Anagram.kt
@@ -1,6 +1,17 @@
 class Anagram(private val word: String) {
 
     fun match(anagrams: Collection<String>): Set<String> {
-        return setOf<String>()
+
+        fun anagramChecker(str1: String, str2: String): Boolean {
+
+            return if(str1 == str2) false
+            else str1.length == str2.length &&
+                    str1.toCharArray().sorted() == str2.toCharArray().sorted()
+
+        }
+
+        return anagrams.filter { anagramChecker(it.toLowerCase(), word.toLowerCase()) }.toSet()
     }
 }
+
+
