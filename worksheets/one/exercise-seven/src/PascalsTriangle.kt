ed6710a95548a92cd2458208fe0ebb0f8f948e0f
vdavios <vas.davios@gmail.com>
2018-01-25 17:51:31 +0000

    Completed Ex07

--- worksheets/one/exercise-seven/src/PascalsTriangle.kt
+++ worksheets/one/exercise-seven/src/PascalsTriangle.kt
@@ -1,7 +1,20 @@
 object PascalsTriangle {
 
     fun computeTriangle(rows: Int): List<List<Int>> {
-        // TODO
-        return listOf(listOf<Int>())
+
+        require(rows>=0) {"Rows can't be negative!"}
+        val listArray = List(rows + 1, { i -> IntArray(i, { 1 }) })
+
+
+        (3..rows).forEach { i ->
+            listArray[i].forEachIndexed { index, _ ->
+                if(index != 0 && index != listArray[i].lastIndex) {
+                    listArray[i][index] = listArray[i - 1][index - 1] + listArray[i - 1][index]
+                }
+            }
+        }
+        return listArray.map { it.toList() }.drop(1)
     }
 }
+
+
