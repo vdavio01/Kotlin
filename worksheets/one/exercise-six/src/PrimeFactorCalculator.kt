f25dc29f8a1b1a4baf08038140c5825f63aa4203
vdavios <vas.davios@gmail.com>
2018-01-25 15:52:37 +0000

    Completed Ex06

--- worksheets/one/exercise-six/src/PrimeFactorCalculator.kt
+++ worksheets/one/exercise-six/src/PrimeFactorCalculator.kt
@@ -1,12 +1,43 @@
 object PrimeFactorCalculator {
 
     fun primeFactors(int: Int): List<Int> {
-        // TODO
-        return listOf<Int>()
+
+        return calculator(int,2)
     }
 
     fun primeFactors(long: Long): List<Long> {
-        // TODO
-        return listOf<Long>()
+
+        return longCalculator(long)
     }
+
+
+    private fun calculator(number: Int, divisor: Int): List<Int> {
+        return when {
+            number % divisor == 0 ->  listOf(divisor) + calculator(number/divisor, divisor)
+
+            number > 1 -> calculator(number, divisor + 1)
+
+            else -> listOf()
+        }
+
+    }
+
+    private fun longCalculator(number: Long): List<Long> {
+        val myList = mutableListOf<Long>()
+        var divisor = 2L
+        var num = number
+        while(num / divisor > 0L) {
+            if(num % divisor == 0L) {
+                num/= divisor
+                myList.add(divisor)
+            } else {
+                divisor ++
+            }
+        }
+
+        return myList
+    }
+
 }
+
+
