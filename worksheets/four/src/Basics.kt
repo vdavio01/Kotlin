961bbc4b703c7b6a027c8bc33be4cc187e1ce438
vdavios <vas.davios@gmail.com>
2018-01-31 14:13:48 +0000

    Completed Worksheet 04 Implemented: add function inRange function oddRange function (int progression) minWhile function minFor function minRecursive function base36 function splitInHalf function isPalindrome function wordCounter function unit test for wordCounter

---
+++ worksheets/four/src/Basics.kt
@@ -0,0 +1,235 @@
+import java.math.BigInteger
+
+/**
+ * This file contains the functions you need
+ * to implement. Please make sure to read the documentation associated
+ * with each function and provide a sensible implementation.
+ */
+
+/**
+ * Write a function called add that takes two Int parameters
+ * and returns their sum.
+ *
+ * @param a operand a
+ * @param b operand b
+ * @return the sum
+ */
+fun add(a: Int, b: Int): Int = a + b
+
+/**
+ * Write a function that returns the inclusive Range from start to end.
+ *
+ * HINT: Look at the Scaladoc for Int/RichInt to find the answer.
+ *
+ * @param start the start of the range
+ * @param end the end of the range
+ * @return the inclusive Range from start to end
+ */
+fun inRange(start: Int, end: Int): IntRange = start..end
+
+/**
+ * Write a function that returns a Range of odd n odd integers starting at 1.
+ *
+ * HINT: Look at the Scaladoc for Int/RichInt to find the answer.
+ *
+ * @param n the number of odd integers in the range
+ * @return a Range of odd integers, excluding the last add integer
+ */
+fun oddRange(n: Int): IntProgression  = 1..n step 2
+
+/**
+ * Write a function that returns the minimum integer in the Array r.
+ *
+ * Your implementation must conform to the following rules:
+ *
+ * - You must use a while loop.
+ * - You may use both immutable (val) and mutable (var) variables.
+ * - You must use an if expression.
+ *
+ * @param r the array of integers
+ * @return the minimum integer in the array
+ */
+fun minWhile(r: Array<Int>): Int {
+    var min = r[0]
+    var i = 0
+    while (i < r.size) {
+        if(r[i] < min) {
+            min = r[i]
+        }
+        i++
+    }
+    return min
+}
+
+/**
+ * Write a function that returns the minimum integer in the Array r.
+ *
+ * Your implementation must conform to the following rules:
+ *
+ * - You must use a for loop (not for comprehension).
+ * - You may use both immutable (val) and mutable (var) variables.
+ * - You may not use an if expression.
+ *
+ * @param r the array of integers
+ * @return the minimum integer in the array
+ */
+fun minFor(r: Array<Int>): Int {
+    var min = r[0]
+
+    for(i in 0 until r.size ) {
+        when {
+            min > r[i] -> min = r[i]
+        }
+    }
+    return min
+}
+
+/**
+ * Write a function called minRecursive that returns the minimum integer in the Array r.
+ *
+ * Your implementation must conform to the following rules:
+ *
+ * - You may not use any loops.
+ * - You may not use any mutable (var) or immutable (val) variables.
+ *
+ * HINT: You might want to look at the Scaladoc for Array to see some
+ * useful methods you can use to solve this.
+ *
+ * @param r the array of integers
+ * @return the minimum integer in the array
+ */
+fun minRecursive(r: Array<Int>): Int {
+    //return r.reduce { x, y -> when{x >= y -> y else -> x} }
+    return when {
+
+        r.size == 1 -> r[0]
+        r[0] >= r[1] -> minRecursive(r.copyOfRange(1,r.size))
+        else -> {
+            r[1] = r[0]
+            minRecursive(r.copyOfRange(1,r.size))
+        }
+    }
+}
+
+/**
+ * Return the base 36 equivalent of the BitInt b.
+ *
+ * HINT: Poke around Scaladoc to find a way of doing this in Scala.
+ *
+ * @param b a big integer
+ * @return the base 36 equivalent
+ */
+fun base36(b: BigInteger): String = b.toString(36)
+
+
+/**
+ * Splits the String s in half.
+ *
+ * This function returns a tuple (f, e), where the f is the first
+ * half of the string and e is the last half of the string.
+ *
+ * For example,
+ *   splitInHalf("abcfun") => ("abc", "fun")
+ *   splitInFalf("abcde")  => ("ab", "cde")
+ *
+ * Your implementation must conform to the following rules:
+ *
+ * - You may not use any loops.
+ * - You may not use recursion.
+ * - You may not use any mutable (var) or immutable (val) variables.
+ *
+ * HINT: You might find something useful in the String and StringOps Scaladoc.
+ *
+ * @param s the string to split
+ * @return the split string as a tuple
+ */
+fun splitInHalf(s: String): Pair<String, String> {
+
+    return Pair(s.substring(0 until s.length/2), s.substring(s.length/2 until s.length))
+    // return when{
+    //    s.length % 2 == 0 -> Pair(s.dropLast(s.length/2), s.drop(s.length/2))
+    //     else -> Pair(s.dropLast(s.length/2 +1), s.drop(s.length/2))
+    // }
+}
+
+/**
+ * Determines if the given string s is a palindrome.
+ *
+ * Your implementation must conform to the following rules:
+ *
+ * - You must use a for comprehension.
+ * - You may not use any other loops.
+ * - You may not use any mutable (var) variables.
+ *
+ * You should normalize the string s before determining if
+ * it is a palindrome. That is, you should not distinguish
+ * between upper and lowercase, you should not consider
+ * spaces, and you should eliminate the punctuation
+ * characters . ? , ; ! - '.
+ *
+ * HINT: You should consult the Scaladoc for String and
+ * StringOps to help you with your solution.
+ *
+ * @param s the potential palindrome
+ * @return true if s is a palindrome; false otherwise
+ */
+fun isPalindrome(s: String): Boolean  {
+    val newS = s.toLowerCase().replace(Regex("\\p{Punct}"),"").replace(Regex("\\s+"),"")
+    return newS == newS.reversed()
+}
+
+/**
+ * You don't have to complete this one as we've removed it from the list
+ * of required functions.
+ *
+ * Sum the characters (as integers) provided as arguments to this method.
+ *
+ * Your implementation must conform to the following rules:
+ *
+ * - You must use a for loop
+ * - You may use any mutable (var) variables.
+ *
+ * @param cc 0 or more characters
+ * @return the sum of the ASCII integers corresponding with the character.
+ */
+//fun sumChars(cc: Char*): Int = 
+
+/**
+ * Counts the number of space delimited words in the provided array of strings.
+ *
+ * This function takes an array of strings that represent lines in a text file.
+ * This function must return a Map from String to Int where the String is a
+ * word found across all lines and the Int is the number of times that word
+ * was seen. For example:
+ *
+ * wordCount(Array("this is a sentence.", "this is a sentence too!"))
+ *
+ * would return
+ *
+ * Map("this" -> 2,
+ *     "is" -> 2,
+ *     "a" -> 2,
+ *     "sentence." -> 1,
+ *     "sentence" -> 1,
+ *     "too!" -> 1)
+ *
+ * You may assume that all words are delimited by spaces. You need not worry
+ * about punctuation. You can implement this however you wish.
+ *
+ * @param lines the lines of a text file
+ * @return a map from words to the number of times that word was seen
+ */
+fun wordCounter(lines: Array<String>): Map<String, Int> {
+
+    val myListOfStrings = mutableListOf<String>()
+    lines.forEach { it.split(Regex("\\s")).forEach { myListOfStrings.add(it) } }
+    return myListOfStrings.groupBy { it }.mapValues { it.value.size }
+
+    // val listOFListOFStrings = mutableListOf<List<String>>()
+    // lines.forEach { listOFListOFStrings.add(it.split(Regex("\\s")) }
+    // listOFListOFStrings.flatMap{ it}.groupBy {it}.mapValues {it.value.size}
+
+}
+
+
+
---
+++ worksheets/four/test/BasicsKtTest.kt
@@ -0,0 +1,146 @@
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+import java.math.BigInteger
+import java.util.*
+
+class BasicsKtTest {
+    lateinit var n: Random
+
+    @Before
+    fun start() {
+        n = Random()
+    }
+
+    /**
+     * The add function should return the addition of a and b
+     */
+    @Test
+    fun addTest() {
+        for (x in 1..1000) {
+            val a = n.nextInt(1000)
+            val b = n.nextInt(1000)
+            val r = add(a, b)
+            assertTrue("Your add function produced the result $r from adding $a to $b", a + b == r)
+        }
+    }
+
+    /**
+     * The inRange function should return an inclusive range
+     */
+    @Test
+    fun inRangeTest() {
+        for (n in 1..1000) {
+            val r = inRange(1, n)
+            assertTrue("The first element in your range is not 1", r.start == 1)
+            assertTrue("The last element in your range is not $n", r.endInclusive == n)
+            // for (v in r) {
+            //   assertTrue("You have a non-odd value $v in your range", v % 2 == 1)
+            //  }
+        }
+    }
+
+    /**
+     * The oddRange function should return a range of all odd integers from 1 to n"
+     */
+    @Test
+    fun oddRangeTest() {
+        for (n in 1..1000) {
+            val r = oddRange(n)
+            assertEquals("The first element in your range is not 1", r.first, 1)
+            for (v in r) {
+                assertEquals("You have a non-odd value $v in your range", v % 2, 1)
+            }
+        }
+    }
+
+    /**
+     * The minWhile function should return the minimum integer in an array using a while loop
+     */
+    @Test
+    fun minWhileTest() {
+        assertEquals(1, minWhile(arrayOf(55, 6, 24, 32, 1, 16, 12)))
+        assertEquals(2, minWhile(arrayOf(2, 3, 4, 5)))
+        assertEquals(12, minWhile(arrayOf(99, 98, 45, 100, 107, 12, 1012, 15, 45, 45, 909)))
+    }
+
+    /**
+     * The minFor function should return the minimum integer in an array using a for loop
+     */
+    @Test
+    fun minForTest() {
+        assertEquals(1, minFor(arrayOf(55, 6, 24, 32, 1, 16, 12)))
+        assertEquals(2, minFor(arrayOf(2, 3, 4, 5)))
+        assertEquals(12, minFor(arrayOf(99, 98, 45, 100, 107, 12, 1012, 15, 45, 45, 909)))
+    }
+
+    /**
+     * The minRecursive function should return the minimum integer in an array using recursion
+     */
+    @Test
+    fun minRecursiveTest() {
+        assertEquals(1, minRecursive(arrayOf(55, 6, 24, 32, 1, 16, 12)))
+        assertEquals(2, minRecursive(arrayOf(2, 3, 4, 5)))
+        assertEquals(12, minRecursive(arrayOf(99, 98, 45, 100, 107, 12, 1012, 15, 45, 45, 909)))
+    }
+
+    /**
+     * The base36 function should return the base 36 equivalent of the given BigInt
+     */
+    @Test
+    fun base36Test() {
+        assertEquals("21i3v9", base36(BigInteger("123456789")))
+        assertEquals("4jc8lik", base36(BigInteger("9876543212")))
+        assertEquals("xrls1yk9rf9", base36(BigInteger("123456789123456789")))
+    }
+
+    /**
+     * The splitInHalf function should split a string in half
+     */
+    @Test
+    fun splitInHalfTest() {
+        fun check(s: String, ls: String, rs: String) = {
+            val (l, r) = splitInHalf(s)
+            assertEquals("$l is not the same as $ls", ls, l)
+            assertEquals("$r is not the same as $rs", rs, r)
+        }
+        check("racecar", "rac", "ecar")
+        check("sloppy", "slo", "ppy")
+    }
+
+    /**
+     * isPalindrome should return true if a string is a palindrome
+     */
+    @Test
+    fun isPalindromeTest() {
+        val pp = arrayOf(
+                "racecar",
+                "A car, a man, a maraca",
+                "A Toyota's a Toyota",
+                "In words, alas, drown I"
+        )
+
+        for (p in pp) {
+            assertTrue("$p is a palindrome", isPalindrome(p))
+        }
+    }
+
+    /**
+     * For you to complete...
+     */
+    @Test
+    fun wordCounter() {
+        //Case sensitive
+        assertEquals(mapOf("vasilis" to 2, "Vasilis" to 1, "davios" to 2, "Davios" to 1)
+                , wordCounter(arrayOf("vasilis Vasilis vasilis", "davios davios Davios")))
+
+
+        assertEquals(mapOf("Vasilis" to 1, "is" to 2, "a" to 2, "student" to 2, "of" to 2, "Msc" to 2, "in" to 2,"Computer" to 2,
+                "Science" to 2, "at" to 2, "Bbk" to 2, "University." to 1, "Davios" to 1, "University!" to 1),
+                wordCounter(arrayOf("Vasilis is a student of Msc in Computer Science at Bbk University.",
+                        "Davios is a student of Msc in Computer Science at Bbk University!")))
+    }
+
+
+}
