3861c4ef4d4ce780929a67fbd7b569ed28f3c794
vasileiosdavios <vas.davios@gmail.com>
2018-10-30 13:52:57 +0000

    Renamed Folder

--- worksheets/one/exercise-one/src/Accumulate.kt
+++ worksheets/one/exercise-one/src/Accumulate.kt
@@ -3,7 +3,6 @@ object Accumulate {
 
         return if(collection.isNotEmpty()) listOf(function(collection[0])) + accumulate(collection.drop(1), function)
         else listOf()
-
         /**
          * Or return collection.map {function(it)} or return collection.map(function)
          */
